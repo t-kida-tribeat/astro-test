40e7e7167d29aa22fa829168b95b629a339650dc
t-kida-tribeat <t-kida@tribeat.com>
2026-02-12 11:34:04 +0900

    GitHub PagesとActionsの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // GitHub Pages: 'placeholder' を自分のGitHubユーザー名に置き換えてください
   site: 'https://t-kida-tribeat.github.io',
-  base: '/opposite-osiris/',
+  base: '/',
 });
